9355e44d1859936fe38de721603732ba5df25d00
Harsh Neel Mani <mani.harshneel@gmail.com>
2025-10-19 23:50:44 +0530

    Updated to use snacks

--- lua/custom/keybindigs.lua
+++ lua/custom/keybindigs.lua
@@ -21,3 +21,38 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 vim.keymap.set('n', '<leader>.', '<C-i>', { desc = 'Move Forward in History' })
 vim.keymap.set('n', '<leader>,', '<C-o>', { desc = 'Move Backward in History' })
 vim.keymap.set('n', '<leader>e', '<cmd>:lua MiniFiles.open()<CR>', { desc = 'Open File [E]xplorer' })
+
+-- NOTE: LSP Definitions
+--
+
+vim.keymap.set('n', 'grd', '<cmd>:lua Snacks.picker.lsp_definitions()<CR>', { desc = '[G]oto [D]efinition' })
+vim.keymap.set('n', 'grD', '<cmd>:lua Snacks.picker.lsp_declarations()<CR>', { desc = '[G]oto [D]eclaration' })
+vim.keymap.set('n', 'grr', '<cmd>:lua Snacks.picker.lsp_references()<CR>', { nowait = true, desc = '[R]eferences' })
+vim.keymap.set('n', 'grI', '<cmd>:lua Snacks.picker.lsp_implementations()<CR>', { desc = '[G]oto [I]mplementation' })
+vim.keymap.set('n', 'gry', '<cmd>:lua Snacks.picker.lsp_type_definitions()<CR>', { desc = '[G]oto T[y]pe Definition' })
+vim.keymap.set('n', '<leader>ss', '<cmd>:lua Snacks.picker.lsp_symbols()<CR>', { desc = 'LSP Symbols' })
+vim.keymap.set('n', '<leader>sS', '<cmd>:lua Snacks.picker.lsp_workspace_symbols()<CR>', { desc = 'LSP Workspace Symbols' })
+
+-- Find things
+vim.keymap.set('n', '<leader>fb', '<cmd>:lua Snacks.picker.buffers() <CR>', { desc = '[B]uffers' })
+vim.keymap.set('n', '<leader>ff', '<cmd>:lua Snacks.picker.files() <CR>', { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fg', '<cmd>:lua Snacks.picker.git_files() <CR>', { desc = '[F]ind [G]it Files' })
+vim.keymap.set('n', '<leader>fr', '<cmd>:lua Snacks.picker.recent() <CR>', { desc = '[R]ecent' })
+vim.keymap.set('n', '<leader>fs', '<cmd>:lua Snacks.picker.smart() <CR>', { desc = '[S]mart Find Files' })
+
+-- search
+vim.keymap.set('n', '<leader>sg', '<cmd>:lua Snacks.picker.grep() <CR>', { desc = 'Grep' })
+vim.keymap.set('n', '<leader>sb', '<cmd>:lua Snacks.picker.grep_buffers() <CR>', { desc = 'Grep Open Buffers' })
+vim.keymap.set('n', '<leader>sR', '<cmd>:lua Snacks.picker.resume() <CR>', { desc = 'Resume' })
+vim.keymap.set('n', '<leader>su', '<cmd>:lua Snacks.picker.undo() <CR>', { desc = 'Undo History' })
+vim.keymap.set('n', '<leader>sd', '<cmd>:lua Snacks.picker.diagnostics() <CR>', { desc = 'Diagnostics' })
+vim.keymap.set('n', '<leader>sH', '<cmd>:lua Snacks.picker.search_history() <CR>', { desc = 'Search History' })
+vim.keymap.set('n', '<leader>sr', '<cmd>:lua Snacks.picker.registers() <CR>', { desc = 'Registers' })
+
+--terminal
+
+--git
+
+vim.keymap.set('n', '<leader>gtl', '<cmd>:lua Snacks.picker.git_log_line() <CR>', { desc = 'Git Log Line' })
+vim.keymap.set('n', '<leader>gtb', '<cmd>:lua Snacks.git.blame_line() <CR>', { desc = 'Git Blame Line' })
+vim.keymap.set('n', '<leader>gtt', '<cmd>:lua Snacks.lazygit() <CR>', { desc = 'Open Lazy Git' })
--- lua/custom/plugins/snacks/init.lua
+++ lua/custom/plugins/snacks/init.lua
@@ -9,17 +9,13 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    -- dashboard = { enabled = true },
     dashboard = dashboard,
-    explorer = { enabled = true },
+    lazygit = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
-    notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
   },
 }
